6693237ed702cc212b117c7d1a4eb965f2fed121
junsu <bobdylan0331@gmail.com>
2023-02-20 17:28:23 +0900

    css semi prj and js studying

---
+++ 03_JAVASCRIPT/js/01_개요.js
@@ -0,0 +1,37 @@
+// 한 줄 주석
+/* 범위 주석 */
+
+// js파일은 <script> 태그 내부라고 생각하면 됨
+
+
+function btnClick2(){
+    alert("external 버튼이 클릭되었습니다.")
+}
+
+function changeColor1(){
+    document.getElementById("box").style.backgroundColor="red";
+}
+
+function changeColor2(){
+    document.getElementById("box").style.backgroundColor="orange";
+}
+
+function changeColor3(){
+    document.getElementById("box").style.backgroundColor="yellow";
+}
+
+function changeColor4(){
+    document.getElementById("box").style.backgroundColor="green";
+}
+
+function changeColor5(){
+    document.getElementById("box").style.backgroundColor="blue";
+}
+
+function changeColor6(){
+    document.getElementById("box").style.backgroundColor="navy";
+}
+
+function changeColor7(){
+    document.getElementById("box").style.backgroundColor="purple";
+}
