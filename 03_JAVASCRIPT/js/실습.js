9758a867df081db694ab6334fe328f58e62c6a5c
junsu <bobdylan0331@gmail.com>
2023-02-23 12:45:43 +0900

    c

--- 03_JAVASCRIPT/js/실습.js
+++ 03_JAVASCRIPT/js/실습.js
@@ -1,9 +1,10 @@
-result = document.getElementById("result");
-
+const result = document.getElementById("result");
+const val1 = document.getElementById("num1");
+const val2 = document.getElementById("num2");
 
 function sum(a, b){
-    a = Number(document.getElementById("num1").value);
-    b = Number(document.getElementById("num2").value);
+    a = Number(val1.value);
+    b = Number(val2.value);
     
     let sum = a+b;
     
@@ -11,8 +12,8 @@ function sum(a, b){
 }
 
 function min(a, b){
-    a = Number(document.getElementById("num1").value);
-    b = Number(document.getElementById("num2").value);
+    a = Number(val1.value);
+    b = Number(val2.value);
     
     let min = a-b;
     
@@ -20,24 +21,25 @@ function min(a, b){
 }
 
 function mul(a, b){
-    a = Number(document.getElementById("num1").value);
-    b = Number(document.getElementById("num2").value);
+    a = Number(val1.value);
+    b = Number(val2.value);
     
     let mul = a*b;
     
     result.innerHTML = "계산 결과 : " + mul;
 }
 function devide(a, b){
-    a = Number(document.getElementById("num1").value);
-    b = Number(document.getElementById("num2").value);
-    
+    a = Number(val1.value);
+    b = Number(val2.value);
+
     let devide = a/b;
     
     result.innerHTML = "계산 결과 : " + devide;
 }
 function remain(a, b){
-    a = Number(document.getElementById("num1").value);
-    b = Number(document.getElementById("num2").value);
+
+    a = Number(val1.value);
+    b = Number(val2.value);
     
     let remain = a%b;
     
